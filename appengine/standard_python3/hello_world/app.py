02e5eb30fc3c7585d9512b8e61e21bbbada49e9f
saxinov <sax.alexander@hotmail.de>
2021-01-29 13:18:31 +0100

    render_template

--- appengine/standard_python3/hello_world/app.py
+++ appengine/standard_python3/hello_world/app.py
@@ -14,7 +14,7 @@
 
 # [START gae_python38_app]
 # [START gae_python3_app]
-from flask import Flask
+from flask import Flask, render_template
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return render_template(homepage.html)
 
 
 if __name__ == '__main__':
